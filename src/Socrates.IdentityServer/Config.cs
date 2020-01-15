55a5a69f0d1514cd3670bbec8bc3409bed0d3b8d
Bruce Mills <bruce.mills@whanautahi.com>
2020-01-15 15:35:49 +1300

    Add project files.

---
+++ src/Socrates.IdentityServer/Config.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Socrates.IdentityServer
+{
+    public static class Config
+    {
+        private const string _clientApiName = "client_api";
+
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[] 
+            {
+                new ApiResource(_clientApiName, "Client API")
+            };
+        
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            {
+                new Client
+                {
+                    ClientId = "socrates_server_side",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    AllowedScopes = { _clientApiName }
+                }
+            };
+        
+    }
+}
